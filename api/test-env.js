cacb6337c457ab15b565813ee5038d146aac4a66
Rex <mailfromrex@gmail.com>
2026-03-16 11:13:49 +0000

    cleanup: remove test-env endpoint

--- api/test-env.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  const key = process.env.STRIPE_SECRET_KEY;
-  return res.status(200).json({
-    hasKey: !!key,
-    keyPrefix: key ? key.substring(0, 7) : null,
-    keyLength: key ? key.length : 0,
-    allEnvKeys: Object.keys(process.env).filter(k => k.startsWith("STRIPE") || k.startsWith("SUPABASE") || k.startsWith("AGENT")),
-  });
-}
